8f42a8c76a7e852a391ce2be1a4a998796cff4d0
Geert Audenaert <geert@mobicage.com>
2017-12-20 16:30:39 +0100

    Improved package script

--- package.sh
+++ package.sh
@@ -2,12 +2,23 @@
 set -e
 ZUTILSBRANCH=master
 JS9BRANCH=master
-ZEROACCESSBRANCH=master
+if [ -z ${1+x} ]
+then 
+    ZEROACCESSBRANCH=master
+else
+    ZEROACCESSBRANCH=$1
+fi
+if [ ${ZEROACCESSBRANCH} = "master" ]
+then
+    TAG="latest"
+else
+    TAG=${ZEROACCESSBRANCH}
+fi
 
 name=c$RANDOM
 dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-docker rmi -f openvcloud/0-access:${ZEROACCESSBRANCH}
+docker rmi -f openvcloud/0-access:${TAG}
 docker run --name ${name} -v ${dir}:/tmp/install ubuntu:16.04 bash /tmp/install/install.sh ${ZUTILSBRANCH} ${JS9BRANCH} ${ZEROACCESSBRANCH}
-docker commit ${name} openvcloud/0-access:${ZEROACCESSBRANCH}
+docker commit ${name} openvcloud/0-access:${TAG}
 docker rm ${name}
